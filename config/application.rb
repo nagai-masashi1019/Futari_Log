3b7da432e3e7458875d8d1ec30f6fa9b994fc86d
永井聖士 <daizuomame0903@gmail.com>
2026-01-12 18:08:46 +0900

    flashメッセージ表示を実装しdeviseの文言を日本語化

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
 require_relative "boot"
 
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Myapp
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.1
 
