8f5099aa34a22c3236298b5e1eb5390c55bc0d12
永井聖士 <daizuomame0903@gmail.com>
2026-02-05 15:59:13 +0900

    カップル解除機能を実装し、紐付けと関連データを削除できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,9 @@ Rails.application.routes.draw do
     end
   end
   resource :settings, only: [ :show ]
-  resource :couple_settings, only: [ :show, :update ]
+  resource :couple_settings, only: [ :show, :update ] do
+    delete :destroy
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
