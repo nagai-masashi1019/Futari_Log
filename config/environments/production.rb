6aecd9285f7b63a2dae216f2fd382c6787e97c0c
永井聖士 <daizuomame0903@gmail.com>
2026-02-15 17:53:45 +0900

    SMTP完全排除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.delivery_method = :test
+
   config.action_mailer.default_url_options = {
     host: "futari-log.onrender.com",
     protocol: "https"
